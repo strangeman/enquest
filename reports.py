2f48dc78fe6d23e38d5797f2a2b7e1f6ced49f69
Markelov Anton <doublic@gmail.com>
2013-09-23 15:46:56 +1100

    create scenario report

--- reports.py
+++ reports.py
@@ -1,9 +1,18 @@
+# -*- coding: UTF-8 -*-
+
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render_to_response
 
+from main.models import Token, Quest, Decision
+
 @user_passes_test(lambda u: u.is_staff, login_url='/admin/', redirect_field_name='')
 def scenario(request):
-    return render_to_response('scenario.tmpl')
+    quest_query = Quest.objects.all()
+    dec_query = Decision.objects.all()
+    template_tree = []
+    for quest in quest_query:
+        template_tree.append({'quest': quest, 'dec_list': dec_query.filter(quest=quest)})
+    return render_to_response('scenario.tmpl', {'tree': template_tree})
 
 @user_passes_test(lambda u: u.is_staff, login_url='/admin/', redirect_field_name='')
 def links(request):
